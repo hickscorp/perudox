ed2c63803f460f3615701b4fee3538fe5a5c6f5b
Pierre Martin <hickscorp@gmail.com>
2020-01-04 20:18:02 +0000

    Updates dependencies.

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Perudox.Mixfile do
       description: description(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
